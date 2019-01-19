d48eb2d0cad5bbb51f585c2e9be72b21d0b65e43
issuser <18772118541@163.com>
2019-01-19 23:00:47 +0800

    成功实现以ajax方式访问后台接口    wxj2090108

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,44 +1,44 @@
 <template>
   <div class="hello">
-        <input type="button" @click="post()" value="test">
+    <input type="button" @click="post(1)" value="查询">
+    <!--  --><div v-for="item in empList">
+    <li>{{ item.name }}，{{ item.age }}，{{ item.phone }},{{ item.userName }}，{{ item.salaryGrade }}，{{ item.identity }}</li>
+    </div>
+
+    <div>
+      分页<input type="button" @click="post()" value='nextPage'> {{ pageIndex }}
+    </div>
   </div>
 </template>
 
 <script>
   import axios from 'axios'
-export default {
-  name: 'HelloWorld',
-  methods: {
-    post: function (){
-      axios.post('/kernel/employee/query', {
+
+  export default {
+    data() {
+      return {//这个里面是定义数据的 第一步
+        empList: 3,
         pageIndex: 1,
         pageSize: 10
-      })
-        .then(function (response) {
-          console.log(response);
+      }
+    },
+    mounted(){
+      this.post() //将方法挂载出来 第三步
+    },
+    methods: {//定义方法
+      post: function (pageIndex){
+        var that=this;
+        axios.post('/kernel/employee/query', {
+          pageIndex: pageIndex,
+          pageSize: 5
         })
-        .catch(function (error) {
-          console.log(error);
-        });
+          .then(function (res) {//获取的数据赋值给上面定义的数据里面 第二步
+            that.empList = res.data.obj.list;
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+      }
     }
   }
-}
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
